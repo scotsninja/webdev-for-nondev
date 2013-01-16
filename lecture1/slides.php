53216b423c9eaa74b860dce860c46b856e7d33d7
Kyle Knox <kknox@sireninteractive.com>
2013-01-16 16:31:08 -0600

    - added extra stuff for reveal.js

--- lecture1/slides.php
+++ lecture1/slides.php
@@ -1,5 +1,18 @@
 <div class="slides">
 	<section>
-		<p>{first slide}</p>
+		<div class="header">header</div>
+		<div class="body">
+			<h1>Title</h1>
+			<h2>Subtitle</h2>
+		</div>
+		<div class="footer">footer</div>
+	</section>
+	<section>
+		<div class="header">header</div>
+		<div class="body">
+			<h1>Title</h1>
+			<h2>Subtitle</h2>
+		</div>
+		<div class="footer">footer</div>
 	</section>
 </div>
